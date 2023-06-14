79eea22a3b2d9eb29c02e53d1e78e84c29502a0e
smidaJaber <js.jeber.smida@gmail.com>
2023-06-14 01:31:04 +0100

    updated project listing

--- src/app/components/Gallery/Projects/Projects.tsx
+++ src/app/components/Gallery/Projects/Projects.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
-
+import {
+	ArrowTopRightOnSquareIcon,
+	ArrowUpOnSquareIcon,
+} from "@heroicons/react/24/solid";
+import { CodeBracketIcon } from "@heroicons/react/24/solid";
 type ProjectsProps = {};
 
 const now = new Date();
@@ -34,7 +38,7 @@ export default function Projects({}: ProjectsProps) {
 						backgroundPosition: "center center",
 						backgroundBlendMode: "multiply",
 						backgroundSize: "cover",
-						transition: "ease-in-out 2s",
+						transition: "ease-in-out 0.7s",
 					}}
 				>
 					<div className="flex flex-col items-center p-8 py-12 text-center dark:text-gray-50 ">
@@ -61,7 +65,7 @@ export default function Projects({}: ProjectsProps) {
 					<div
 						onMouseOver={() => {
 							handleChangeMainImage(
-								"https://media.mehrnews.com/d/2020/04/25/4/3435085.jpg"
+								"https://i.ibb.co/SQ2BQrz/Capture-d-cran-2023-06-14-001509.png"
 							);
 						}}
 						onMouseLeave={handleResetMainImage}
@@ -69,6 +73,7 @@ export default function Projects({}: ProjectsProps) {
 					>
 						<span>12 June 2020</span>
 						<h1 className="text-3xl font-bold">Insurance Agency</h1>
+
 						<p>
 							this a website for an insurance agency that handles the agency
 							presentation; in term of services, achievements, etc. As well as
@@ -94,6 +99,54 @@ export default function Projects({}: ProjectsProps) {
 								></path>
 							</svg>
 						</a>
+						<div className="group top-0 left-0  flex items-start justify-start   ">
+							<div className="text-white   flex items-center justify-center p-1  rounded-md  bg-green-900 z-50 absolute  ">
+								Skills
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-16   flex  p-2 hover:p-3 bg-green-300 scale-100 hover:bg-green-400 text-white">
+								HTML
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-24 group-hover:-translate-y-10  flex  p-2 hover:p-3 bg-yellow-600 scale-100 hover:bg-green-400 text-white">
+								Mantine
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-16  flex  p-2 hover:p-3 bg-blue-300 hover:bg-blue-400  text-white">
+								CSS
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-14 group-hover:translate-x-14   flex  p-2 hover:p-3 bg-yellow-400 hover:bg-yellow-400 text-black font-extrabold">
+								Javascript
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:-translate-y-14 group-hover:translate-x-18   flex  p-2 hover:p-3 bg-green-800 hover:bg-green-800 text-white">
+								Supabase
+							</div>
+						</div>
+						<div className="inline-flex items-center py-2 space-x-2 float-right ">
+							<a
+								className="inline-flex items-center px-2 mx-2 space-x-2 text-sm  text-blue-900 hover:border-white border-black border-2  rounded-md p-1 m-1"
+								href="https://github.com/smidaJaber/insurance-agency"
+								target="_blank"
+							>
+								Source{" "}
+								<CodeBracketIcon
+									className="ml-2 h-5 w-5 right-0"
+									title="View source code"
+								/>
+							</a>
+							<a
+								className="inline-flex items-center px-2 mx-2 space-x-2 text-sm  text-blue-900 hover:border-white border-black border-2  rounded-md p-1 m-1"
+								href="https://insurance-agency-js.vercel.app/"
+								target="_blank"
+							>
+								Live
+								<ArrowTopRightOnSquareIcon
+									fontSize={"7px"}
+									className="ml-2 sh-5 w-5"
+									title="Preview project"
+								/>
+							</a>
+						</div>
 					</div>
 					<div
 						onMouseOver={() => {
@@ -126,6 +179,43 @@ export default function Projects({}: ProjectsProps) {
 								></path>
 							</svg>
 						</a>
+						<div className="group top-0 left-0  flex items-start justify-start   ">
+							<div className="text-white   flex items-center justify-center p-1  rounded-md  bg-green-900 z-50 absolute  ">
+								Skills
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-16   flex  p-2 hover:p-3 bg-green-300 scale-100 hover:bg-green-400 text-white">
+								HTML
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-24 group-hover:-translate-y-10  flex  p-2 hover:p-3 bg-yellow-600 scale-100 hover:bg-green-400 text-white">
+								MySQL
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-16  flex  p-2 hover:p-3 bg-blue-300 hover:bg-blue-400  text-white">
+								CSS
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-14 group-hover:translate-x-14   flex  p-2 hover:p-3 bg-yellow-400 hover:bg-yellow-400 text-black font-extrabold">
+								Javascript
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:-translate-y-14 group-hover:translate-x-18   flex  p-2 hover:p-3 bg-blue-800 hover:bg-blue-800 text-white">
+								ElectronJs
+							</div>
+						</div>
+						<div className="inline-flex items-center py-2 space-x-2 float-right ">
+							<a
+								className="inline-flex items-center px-2 mx-2 space-x-2 text-sm  text-blue-900 hover:border-white border-black border-2  rounded-md p-1 m-1"
+								href="#"
+								target="_blank"
+							>
+								Work In progress
+								<ArrowUpOnSquareIcon
+									fontSize={"7px"}
+									className="ml-2 h-6 w-6"
+									title="Preview project"
+								/>
+							</a>
+						</div>
 					</div>
 					<div
 						onMouseOver={() => {
@@ -138,7 +228,7 @@ export default function Projects({}: ProjectsProps) {
 					>
 						<span>12 June</span>
 						<h1 className="text-3xl font-bold">
-							Grau! An electron app to manage Agriculture inventory
+							Grau! An electron+Reactjs app to manage Agriculture inventory
 						</h1>
 						<p>
 							this app started as a local business for an inventory that
@@ -147,7 +237,7 @@ export default function Projects({}: ProjectsProps) {
 						<a
 							rel="noopener noreferrer"
 							href="#"
-							className="inline-flex items-center py-2 space-x-2 text-sm dark:text-violet-400"
+							className="inline-flex items-center   space-x-2 text-sm dark:text-violet-400"
 						>
 							<span>Read more</span>
 							<svg
@@ -163,6 +253,44 @@ export default function Projects({}: ProjectsProps) {
 								></path>
 							</svg>
 						</a>
+						<div className="group top-0 left-0  flex items-start justify-start   ">
+							<div className="text-white   flex items-center justify-center p-1  rounded-md  bg-green-900 z-50 absolute  ">
+								Skills
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-16   flex  p-2 hover:p-3 bg-green-300 scale-100 hover:bg-green-400 text-white">
+								HTML
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-y-0 group-hover:scale-y-100 group-hover:translate-x-24 group-hover:-translate-y-10  flex  p-2 hover:p-3 bg-yellow-600 scale-100 hover:bg-green-400 text-white">
+								MySQL
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-16  flex  p-2 hover:p-3 bg-blue-300 hover:bg-blue-400  text-white">
+								CSS
+							</div>
+
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:translate-y-14 group-hover:translate-x-14   flex  p-2 hover:p-3 bg-yellow-400 hover:bg-yellow-400 text-black font-extrabold">
+								Javascript
+							</div>
+							<div className="absolute rounded-full transition-all duration-[0.2s] ease-out scale-x-0 group-hover:scale-x-100 group-hover:-translate-y-14 group-hover:translate-x-18   flex  p-2 hover:p-3 bg-blue-800 hover:bg-blue-800 text-white">
+								ElectronJs
+							</div>
+						</div>
+
+						<div className="inline-flex items-center py-2 space-x-2 float-right ">
+							<a
+								className="inline-flex items-center px-2 mx-2 space-x-2 text-sm  text-blue-900 hover:border-white border-black border-2  rounded-md p-1 m-1"
+								href="#"
+								target="_blank"
+							>
+								Work In progress
+								<ArrowUpOnSquareIcon
+									fontSize={"7px"}
+									className="ml-2 h-6 w-6"
+									title="Preview project"
+								/>
+							</a>
+						</div>
 					</div>
 				</div>
 			</div>

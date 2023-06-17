1f16d5499cf91e92501f1e7caf64e97d30d164be
smidaJaber <js.jeber.smida@gmail.com>
2023-06-17 13:59:48 +0100

    add GTag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 import "./globals.css";
@@ -8,7 +9,10 @@ export const metadata = {
 	title: "Bensmida Jaber - Portfolio",
 	description: "my portfolio",
 };
-
+const isProd = process.env.NODE_ENV === "production";
+const gtag = isProd
+	? `https://www.googletagmanager.com/gtag/js?id=G-RZ71FLN5HY`
+	: "";
 export default function RootLayout({
 	children,
 }: {
@@ -16,6 +20,23 @@ export default function RootLayout({
 }) {
 	return (
 		<div>
+			<Head>
+				<script async src={gtag} />
+				<script
+					id="google-analytics"
+					defer
+					dangerouslySetInnerHTML={{
+						__html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-RZ71FLN5HY', {
+                  page_path: window.location.pathname
+                });
+              `,
+					}}
+				/>
+			</Head>
 			<div className={inter.className}>
 				<Navbar />
 
